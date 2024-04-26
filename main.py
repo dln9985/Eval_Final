d982f512848b7dc570d1a9b7f3cf27507b0bcd3e
Diego López Naujokat <diego.l@uteca.edu.mx>
2024-04-25 21:00:56 -0600

    Commit final de proyecto final

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import threading 
+import threading
 import time
 import pandas as pd
 
-print("")
-
-def LecturaArchivo(archivo):
+def leer_archivo(archivo):
     Sheet = pd.read_excel(archivo, header=None, index_col=None)
     numeros = Sheet.values.flatten()
-    print("Todos los números contenidos en el archivo:")
+    
+    print("Todos los números en el archivo:")
     for i in range(0, len(numeros), 20):
         for j in range(i, min(i + 20, len(numeros))):
             numero = numeros[j]
             print(f"{numero:6d}", end='')
         print()
-    print()  
+    
+    print()
     return numeros
 
-def calcular_suma_parcial(numeros, inicio, fin, resultado):
+def suma_parcial(numeros, inicio, fin, resultado):
     suma = 0
     for i in range(inicio, fin):
         suma += numeros[i]
@@ -29,11 +29,14 @@ def suma_total(numeros, num_hilos):
     sumas_parciales = []
     inicio = 0
 
-    print("\nCantidad de hilos a emplear:", num_hilos)
+    print("")
+    print(f"Cantidad de hilos a emplear: {num_hilos}")
+    
     for i in range(num_hilos):
         fin = inicio + tamano_por_hilo if i < tamano % num_hilos else inicio + tamano_por_hilo
-        print("Hilo", 1+i, ":", inicio + 1, "-", fin) 
-        hilo = threading.Thread(target=calcular_suma_parcial, args=(numeros, inicio, fin, sumas_parciales))
+        print(f"Hilo {i+1}: {inicio + 1} - {fin}") 
+        
+        hilo = threading.Thread(target=suma_parcial, args=(numeros, inicio, fin, sumas_parciales))
         hilos.append(hilo)
         inicio = fin
 
@@ -46,8 +49,8 @@ def suma_total(numeros, num_hilos):
         hilo.join()
 
     tiempo_de_ejecucion = time.time() - start_time
-    suma_total = sum(sumas_parciales)
-    return tiempo_de_ejecucion, suma_total
+
+    return sum(sumas_parciales), tiempo_de_ejecucion, sumas_parciales
 
 if __name__ == "__main__":
     while True:
@@ -55,9 +58,9 @@ if __name__ == "__main__":
         if nombre_archivo == "datos-Eval-Final.xlsx":
             break
         else:
-            print("El archivo que se solicita no esta disponible. Intente nuevamente.")
+            print("El archivo que se solicita no está disponible. Intente nuevamente.")
 
-    numeros = LecturaArchivo(nombre_archivo)
+    numeros = leer_archivo(nombre_archivo)
         
     if numeros is not None and len(numeros) > 0:
         while True:
@@ -67,8 +70,12 @@ if __name__ == "__main__":
                 if 2 <= num_hilos <= 10:
                     break
 
-        tiempo_ejecucion, suma_total = suma_total(numeros, num_hilos)
+        suma_total, tiempo_ejecucion, sumas_parciales = suma_total(numeros, num_hilos)
 
+        print("")
+        print("Sumas parciales de cada hilo:")
+        for i in range(len(sumas_parciales)):
+            print(f"Hilo {i+1}: {sumas_parciales[i]}")
         print("")
         print("Suma Total:", suma_total)
         print("Tiempo de ejecución:", tiempo_ejecucion)
